1454727d2b3df803d99a48cbd7a2155a3656a1f7
garyhull123 <64485941+garyhull123@users.noreply.github.com>
2025-07-12 08:44:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Tradefest site loaded');
+
+function moveHorse() {
+  const who = document.getElementById('who').value;
+  const distance = parseInt(document.getElementById('distance').value);
+  const horse = document.querySelector(`#${who} .horse`);
+  const currentLeft = parseInt(horse.style.left || 0);
+  horse.style.left = (currentLeft + distance) + 'px';
+}
