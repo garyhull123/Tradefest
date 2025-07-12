e2c4936759ab2ad9d5ee151171e07ab275a33b2f
garyhull123 <64485941+garyhull123@users.noreply.github.com>
2025-07-12 14:16:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Horse race logic goes here');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const comments = [
+    "Matt’s off to a flyer!",
+    "Dan is galloping through the ranks!",
+    "Danny's gaining ground!",
+    "Ryan's kicking into gear!"
+  ];
+  let index = 0;
+  setInterval(() => {
+    document.getElementById('bookie-comment').innerText = comments[index];
+    index = (index + 1) % comments.length;
+  }, 3000);
+});
