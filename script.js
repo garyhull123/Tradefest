6d1c3e7884b7ff68a10c279719deab4cf3a8b05c
garyhull123 <64485941+garyhull123@users.noreply.github.com>
2025-07-12 14:40:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => { console.log('Race started!'); });
+
+function moveHorse(horseNum) {
+    const input = document.getElementById('input' + horseNum);
+    const value = Math.min(Math.max(parseInt(input.value) || 0, 0), 100);
+    const horse = document.getElementById('horse' + horseNum);
+    horse.style.left = value + '%';
+}
