9f1077910e6f2d3b707ad3fd783a6ec6c40c0773
garyhull123 <64485941+garyhull123@users.noreply.github.com>
2025-07-13 14:54:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function updateHorse(name) {
-  const inputVal = document.getElementById(name + "Val").value;
-  const horse = document.getElementById(name);
-  horse.style.transform = "translateX(" + inputVal + "px)";
-}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lines = [
+    "Matt’s got his hat in the game!",
+    "Dan’s cooking up a win!",
+    "Danny’s hammering the lead!",
+    "Ryan’s flying like Maverick!"
+  ];
+
+  setInterval(() => {
+    const oneLiner = lines[Math.floor(Math.random() * lines.length)];
+    document.getElementById('one-liner').textContent = oneLiner;
+  }, 4000);
+});
