ccf651c0ac1cf482f201fee639204ce3d3cfd1c6
garyhull123 <64485941+garyhull123@users.noreply.github.com>
2025-07-14 10:38:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const lines = [
-    "Matt’s got his hat in the game!",
-    "Dan’s cooking up a win!",
-    "Danny’s hammering the lead!",
-    "Ryan’s flying like Maverick!"
-  ];
-
-  setInterval(() => {
-    const oneLiner = lines[Math.floor(Math.random() * lines.length)];
-    document.getElementById('one-liner').textContent = oneLiner;
-  }, 4000);
-});
+console.log('Tradefest site loaded');
