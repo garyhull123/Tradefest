b0f66db1cbef788a61fed0e9d9d2fc37810e2ac9
garyhull123 <64485941+garyhull123@users.noreply.github.com>
2025-07-14 14:16:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// Placeholder for script.js
+// This will later be used to update horse positions from Google Sheets data
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Race started...");
+});
